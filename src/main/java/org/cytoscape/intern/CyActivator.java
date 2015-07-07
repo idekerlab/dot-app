eaf93556e8f0cefe3d1b5a2a01683125977184cb
Massoud Maher <mahermassoud@gmail.com>
2015-07-07 14:51:49 -0700

    Skeleton code for CyActivator

--- src/main/java/org/cytoscape/intern/CyActivator.java
+++ src/main/java/org/cytoscape/intern/CyActivator.java
@@ -1,6 +1,29 @@
 package org.cytoscape.intern;
-import org.cytoscape.*;
- 
-public class CyActivator {
+
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Runs the program
+ * 
+ * @author Massoud Maher
+ * @author Braxton Fitts
+ * @author Ziran Zhang
+ */
+public class CyActivator extends AbstractCyActivator{
+	
+	/**
+	 * Method that runs when class is activated-- will start dot app
+	 * 
+	 * @param context the OSGi context in which this class is activated
+	 */
+	@Override
+	public void start(BundleContext context) {
+		
+		// CyApplicationManager = getService()
+		// DotWriterFactory = getService()
+		// CyFileFilter = getService()
+
+	}
 
 }
