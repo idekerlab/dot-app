6860fc3b18d440a44b1f5e7dd54831ff153d457f
Ziran Zhang <Sean@Zirans-MacBook-Air.local>
2015-07-08 10:51:02 -0700

    DataManager

---
+++ src/main/java/org/cytoscape/intern/DataManager.java
@@ -0,0 +1,56 @@
+package org.cytoscape.intern;
+
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.view.model.View;
+import org.cytoscape.view.model.CyNetworkView;
+
+
+/**
+ * An instance of manager, which is constructed in 
+ * DotWriteTask class. Calls methods which return a 
+ * property String based on the passed in View.  
+ * 
+ * @author Massoud Maher
+ * @author Braxton Fitts
+ * @author Ziran Zhang
+ */
+public class DataManager{
+  
+  //instance of NodePropertyMapper class
+  NodePropertyMapper nodeMapper;
+
+  //instance of EdgePropertyMapper class
+  EdgePropertyMapper edgeMapper;
+
+  
+  /*
+   * checks out which method,either getEdgeString or getNodeString, 
+   * will be called based on the parameter 
+   *
+   * @param pass in the View (Edge's or Node's), 
+   * which determines Edge property or Node property
+   */
+  public String getElementString (View <Object>) {
+    //TODO
+  }
+ 
+  /*
+   * Calls method mapVisToDot in EdgePropertyManager class 
+   *
+   * @param pass in the Edge View
+   */
+  public String getEdgeString(View<CyEdge>){
+    //TODO
+  }
+
+  /*
+   * Calls method mapVisToDot in NodePropertyManager class 
+   *
+   * @param pass in the Node View
+   */
+   public String getNodeString(View<CyNode>){
+     //TODO
+   }
+
+}
