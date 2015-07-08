dfc5422420ce3a53b29e93807549def894e52010
Massoud Maher <mahermassoud@gmail.com>
2015-07-08 10:20:28 -0700

    Almost finished Mapper class, formatting in CyActivator. Must add access levels of instance variables.

--- src/main/java/org/cytoscape/intern/Mapper.java
+++ src/main/java/org/cytoscape/intern/Mapper.java
@@ -1,10 +1,15 @@
 package org.cytoscape.intern;
 
 import java.util.HashMap;
+import java.awt.Color;
+import java.awt.Font;
 import org.cytoscape.view.presentation.property.LineTypeVisualProperty;
+import org.cytoscape.view.model.VisualProperty;
 
 /**
- * Runs the program
+ * Handles mapping of Cytoscape properties to .dot attributes in the form of a String.
+ * Contains implementation for properties that are shared by nodes and edges and declarations
+ * for unshared properties. 
  * 
  * @author Massoud Maher
  * @author Braxton Fitts
@@ -12,6 +17,7 @@ import org.cytoscape.view.presentation.property.LineTypeVisualProperty;
  */
 public abstract class Mapper {
 
+	// TODO -- access level of 3 variables below
 	// maps Cytoscape properties  by their ID Strings to their .dot equivalents if relationship is simple equivalency
 	HashMap<String, String> simpleVisPropsToDot; // TODO fill in
 	
@@ -19,13 +25,54 @@ public abstract class Mapper {
 	 * maps Cytoscape VisualProperty TYPES by their String ID to a HashMap that contains the 
 	 * cytoscape to *.dot mappings for that type
 	 */
-	HashMap<String, HashMap<Object, Object> >discreteMappingTypes;
+	HashMap<String, HashMap<Object, Object> >discreteMappingTypes; // TODO
 	
 	// maps Cytoscape line types to the equivalent string used in .dot
-	HashMap<LineTypeVisualProperty, String> lineTypeMap;
+	HashMap<LineTypeVisualProperty, String> lineTypeMap; // TODO
 	
 	
 	/**
+	 * Takes a VisualProperty and returns the String put in the .dot file to represent it
 	 * 
+	 * @param property VisualProperty to be converted
+	 * @return String that is .dot equivalent of the visual property
 	 */
+	public String mapVisToDot(VisualProperty<Object> property) {
+		//TODO
+		return null;
+	}
+	
+	/**
+	 * Given a color, returns the color in String format that .dot uses for color.
+	 * Format is "#%rr%gg%bb%aa" -- red, green, blue, alpha in hexadecimal
+	 * 
+	 * @param color color being converted
+	 * @param alpha alpha level of that color-- cytoscape does not use alpha in Paint class
+	 * @return String representation of color in .dot format of rgba
+	 */
+	public String mapColorToDot(Color color, Integer alpha) {
+		// TODO
+		return null;
+	}
+	
+	/**
+	 * Given a font, returns the .dot equivalent in String form
+	 * 
+	 * @param font font to be converted
+	 * @param size size of font to be converted
+	 * @return String that is .dot representation of the provided font
+	 */
+	public String mapFont(Font font, Integer size) {
+		// TODO
+		return null;
+	}
 }
+
+
+
+
+
+
+
+
+
